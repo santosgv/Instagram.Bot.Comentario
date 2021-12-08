1f5ded06d014a52857265b9d58e65e0038dad0cf
Vitor <santosgomesv@gmail.com>
2021-12-08 12:11:55 -0300

    Interface visual do usuario

---
+++ Project/interface.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from Project.main import Instagem
+
+
+main = Tk()
+
+
+class Logar():
+    def logar(self):
+        self.bot=Instagem()
+        self.bot.login(self.login.get(),self.password.get())
+        self.linkfoto()
+        self.comenta()
+
+    def linkfoto(self):
+        self.bot.Postagem(self.LinkInstagram.get())
+
+    def comenta(self):
+        qTd=int(self.QtdComentarios.get())
+        iVcS=int(self.IntervaloComentariosSeg.get())
+        self.bot.Comenta(qTd,iVcS)
+
+class Aplicacao(Logar):
+    def __init__(self):
+        self.main=main
+        self.Principal()
+        self.Frame()
+        self.Label()
+        main.mainloop()
+
+    def Principal(self):
+        self.main.title('Boot comentario instagram')
+        self.main.geometry('800x500')
+        self.main.resizable(False, False)
+
+    def Frame(self):
+        self.FramePrincipal = Frame(self.main, bg='#F8F8FF', highlightbackground='black')
+        self.FramePrincipal.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.98)
+
+
+    def Label(self):
+        Label(self.FramePrincipal, text='Login', bg='white',fg='#B22222').place(relx=0.08, rely=0.01)
+        self.login = Entry(self.FramePrincipal)
+        self.login.place(relx=0.15, rely=0.01,width=300)
+
+        Label(self.FramePrincipal, text='Senha', bg='white' ,fg='#B22222').place(relx=0.08, rely=0.10)
+        self.password = Entry(self.FramePrincipal,show="*")
+        self.password.place(relx=0.15, rely=0.10)
+
+        Label(self.FramePrincipal,text='Link do instagram' ,bg='white',fg='#B22222').place(relx=0.08,rely=0.20)
+        self.LinkInstagram=Entry(self.FramePrincipal)
+        self.LinkInstagram.place(relx=0.30, rely=0.20,width=500)
+
+        Label(self.FramePrincipal,text='Quantidade de comentarios' ,bg='white',fg='#B22222').place(relx=0.08,rely=0.30)
+        self.QtdComentarios=Entry(self.FramePrincipal)
+        self.QtdComentarios.place(relx=0.30,rely=0.30 ,width=50)
+
+        Label(self.FramePrincipal, text='Intervalo entre comentarios em Segundos', bg='white',fg='#B22222').place(relx=0.08, rely=0.40)
+        self.IntervaloComentariosSeg=Entry(self.FramePrincipal)
+        self.IntervaloComentariosSeg.place(relx=0.40, rely=0.40,width=50)
+
+        self.BtnLogareComentar = Button(self.main, text='Logar', command=self.logar)
+        self.BtnLogareComentar.place(relx=0.5, rely=0.60)
